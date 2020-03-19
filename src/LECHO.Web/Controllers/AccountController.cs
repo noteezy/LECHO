d7ea255b0a66f1276fedaf9fda909b354bdc90f7
NazarHR <47197105+NazarHR@users.noreply.github.com>
2020-03-19 22:07:49 +0200

    Access to site (#33)

---
+++ src/LECHO.Web/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LECHO.Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [Authorize(Roles ="1")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
