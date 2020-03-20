86f0ec0032b4a9f5f7575f90ff4fc143e347fbcf
Vlada Baranovych <martabaranovuch@gmail.com>
2020-03-20 19:44:06 +0200

    Fix login page + profile page v1.0 + diff layouts (#34)
    * Login Page Style v1.1
    
    * fix login page + profile v1.0 + diff layouts for pages(not done yet)
    
    Co-authored-by: NazarHR <47197105+NazarHR@users.noreply.github.com>

--- src/LECHO.Web/Controllers/AccountController.cs
+++ src/LECHO.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LECHO.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,26 @@ namespace LECHO.Web.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        [Authorize(Roles ="1")]
+        [Authorize]
         public IActionResult Profile()
         {
+            var map = new Dictionary<string, string>();
+            map.Add("1", "Адмін");
+            map.Add("2", "Викладач");
+            map.Add("3", "Студент");
+
+            var user = AccountAccess.GetUser(User.Identity.Name);
+            ViewData["FirstName"] = user.FirstName;
+            ViewData["LastName"] = user.LastName;
+            ViewData["MiddleName"] = user.MiddleName;
+            ViewData["Role"] = map[user.Role.ToString()];
             return View();
         }
+        //[HttpPost]
+        //public IActionResult Logout()
+        //{
+        //    HttpContext.Session.Clear();
+        //    return RedirectToAction("Login", "Login");
+        //}
     }
 }
