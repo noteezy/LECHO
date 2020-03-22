e6dbf0619573ffd5c3eb7423fc587c3d27964594
Sofia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2020-03-22 21:35:36 +0200

    Subjects view (#39)
    * Subjects view
    
    closes #30

--- src/LECHO.Web/Controllers/AccountController.cs
+++ src/LECHO.Web/Controllers/AccountController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LECHO.Core;
+using LECHO.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using LECHO.Infrastructure;
+
 
 namespace LECHO.Web.Controllers
 {
@@ -21,7 +24,7 @@ namespace LECHO.Web.Controllers
             map.Add("2", "Викладач");
             map.Add("3", "Студент");
 
-            var user = AccountAccess.GetUser(User.Identity.Name);
+            var user = AccountManagement.GetUser(User.Identity.Name);
             ViewData["FirstName"] = user.FirstName;
             ViewData["LastName"] = user.LastName;
             ViewData["MiddleName"] = user.MiddleName;
---
+++ src/LECHO.Web/Controllers/SubjectsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LECHO.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using LECHO.Infrastructure;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LECHO.Web.Controllers
+{
+    [Authorize]
+    public class SubjectsController : Controller
+    {
+        [Authorize]
+        public ViewResult Subjects()
+        {
+            var map = new Dictionary<string, string>();
+            map.Add("1", "Адмін");
+            map.Add("2", "Викладач");
+            map.Add("3", "Студент");
+            var user = AccountManagement.GetUser(User.Identity.Name);
+            Subjects[] subjectsList;
+            ViewData["Information"] = "";
+            if (user.Role == 3)
+            {
+                var student = AccountManagement.GetStudent(user.UserId);
+                if (student.Course == 1) 
+                {
+                    subjectsList = new List<Subjects>()
+                    .Concat(SubjectManagement.GetSubjects(3))
+                    .Concat(SubjectManagement.GetSubjects(4))
+                    .ToArray(); 
+                }
+                else if (student.Course == 2) 
+                {
+                    subjectsList = new List<Subjects>()
+                    .Concat(SubjectManagement.GetSubjects(5))
+                    .Concat(SubjectManagement.GetSubjects(6))
+                    .ToArray();
+                } 
+                else 
+                {
+                    subjectsList = SubjectManagement.GetSubjects(1);
+                    ViewData["Information"] = "Вибіркові дисципліни для вас не опубліковані.";
+                }
+                
+            } 
+            else 
+            {
+                subjectsList = new List<Subjects>()
+                .Concat(SubjectManagement.GetSubjects(3))
+                .Concat(SubjectManagement.GetSubjects(4))
+                .Concat(SubjectManagement.GetSubjects(5))
+                .Concat(SubjectManagement.GetSubjects(6))
+                .ToArray();
+            }
+            return View(subjectsList);
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
