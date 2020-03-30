00247b7f89c5689c1053d978d5b6bc29797ec760
Sofia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2020-03-30 17:13:08 +0300

    Add new views to Subjects pages

--- src/LECHO.Web/Controllers/SubjectsController.cs
+++ src/LECHO.Web/Controllers/SubjectsController.cs
@@ -13,7 +13,7 @@ namespace LECHO.Web.Controllers
     public class SubjectsController : Controller
     {
         [Authorize]
-        public ViewResult Subjects()
+        public ViewResult SubjectsFirstTerm()
         {
             var map = new Dictionary<string, string>();
             map.Add("1", "Адмін");
@@ -27,17 +27,11 @@ namespace LECHO.Web.Controllers
                 var student = AccountManagement.GetStudent(user.UserId);
                 if (student.Course == 1) 
                 {
-                    subjectsList = new List<Subjects>()
-                    .Concat(SubjectManagement.GetSubjects(3))
-                    .Concat(SubjectManagement.GetSubjects(4))
-                    .ToArray(); 
+                    subjectsList = SubjectManagement.GetSubjects(3);
                 }
                 else if (student.Course == 2) 
                 {
-                    subjectsList = new List<Subjects>()
-                    .Concat(SubjectManagement.GetSubjects(5))
-                    .Concat(SubjectManagement.GetSubjects(6))
-                    .ToArray();
+                    subjectsList = SubjectManagement.GetSubjects(5);
                 } 
                 else 
                 {
@@ -50,8 +44,43 @@ namespace LECHO.Web.Controllers
             {
                 subjectsList = new List<Subjects>()
                 .Concat(SubjectManagement.GetSubjects(3))
-                .Concat(SubjectManagement.GetSubjects(4))
                 .Concat(SubjectManagement.GetSubjects(5))
+                .ToArray();
+            }
+            return View(subjectsList);
+        }
+
+        public ViewResult SubjectsSecondTerm()
+        {
+            var map = new Dictionary<string, string>();
+            map.Add("1", "Адмін");
+            map.Add("2", "Викладач");
+            map.Add("3", "Студент");
+            var user = AccountManagement.GetUser(User.Identity.Name);
+            Subjects[] subjectsList;
+            ViewData["Information"] = "";
+            if (user.Role == 3)
+            {
+                var student = AccountManagement.GetStudent(user.UserId);
+                if (student.Course == 1)
+                {
+                    subjectsList = SubjectManagement.GetSubjects(4);
+                }
+                else if (student.Course == 2)
+                {
+                    subjectsList = SubjectManagement.GetSubjects(6);
+                }
+                else
+                {
+                    subjectsList = SubjectManagement.GetSubjects(1);
+                    ViewData["Information"] = "Вибіркові дисципліни для вас не опубліковані.";
+                }
+
+            }
+            else
+            {
+                subjectsList = new List<Subjects>()
+                .Concat(SubjectManagement.GetSubjects(4))
                 .Concat(SubjectManagement.GetSubjects(6))
                 .ToArray();
             }
