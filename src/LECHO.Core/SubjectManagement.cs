520928d727eb0e640fceefe907864bd37f02bf95
Sofia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2020-04-07 16:12:28 +0300

    Subjects view upgrade
    * Subjects view
    
    closes #30
    
    * Subjects View (bugs fix)
    
    closes #30
    
    * Add new views to Subjects pages
    
    * Add new views to Subjects pages (with changes)
    
    Виправлено синтаксичну помилку у повідомленні однієї з функцій AccountManagment
    Прибрано зайвий код
    Змінено назву незрозумілого класу
    Закрито #41
    
    * Added SearchBox
    
    Додано функцію пошуку предметів по назві та відповідне відображення на сторінці
    
    * Add FavouriteSubjects pages
    
    - додано сторінки з обраними предметами
    - додано візуальне відображення обраних на сторінці з предметами
    
    * Refactored getFavouriteSubjects method

--- src/LECHO.Core/SubjectManagement.cs
+++ src/LECHO.Core/SubjectManagement.cs
@@ -11,17 +11,22 @@ namespace LECHO.Core
         {
             database = dm;
         }
-        public Subjects[] GetSubjects(int semester)
-        {
-            var subject = database.Subjects.ToArray().Where(s => s.Semester == semester).Select(s => s).ToArray();
-            return subject;
-        }
         public Faculties GetFaculty(int id)
         {
             var faculty = database.Faculties.FirstOrDefault(u => u.FacultyId == id);
             return faculty;
         }
 
+        public Favourites GetFavourite(int id)
+        {
+            var subject = database.Favourites.FirstOrDefault(u => u.SubjectId == id);
+            return subject;
+        }
+        public Subjects[] GetSubjects(int semester)
+                {
+                    var subject = database.Subjects.ToArray().Where(s => s.Semester == semester).Select(s => s).ToArray();
+                    return subject;
+                }
         public Subjects[] GetSubjects(string title) { return null; }
         public Subjects[] GetSubjectsByTitle(string title, Subjects[] subjects)
         {
@@ -29,7 +34,16 @@ namespace LECHO.Core
             var selectedSubjects = subjects.Where(r => titleParts.Any(t => r.Name.Contains(t))).Select(r => r).ToArray();
             return selectedSubjects;
         }
-
+        public Subjects[] GetFavouriteSubjects(int _UserId, int semester)
+        {
+            var subjectsList = database.Subjects
+                .Where(s => (database.Favourites.Where(f=>f.UserId==_UserId)
+                .Any(f => f.SubjectId == s.SubjectId)))
+                .Where(s=>s.Semester==semester)
+                .Select(s=>s)
+                .ToArray();
+            return subjectsList;
+        }
         public void AddSubjectToFavourite(int _UserId, int _SubjId)
         {
             database.Favourites.Add(new Favourites { UserId = _UserId, SubjectId = _SubjId });
