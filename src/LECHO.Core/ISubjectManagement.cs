aa9327d7b5f560baeeb4c6e76cb9aa062a7d578b
Vlada Baranovych <martabaranovuch@gmail.com>
2020-04-07 17:21:07 +0300

    Minor fix with FavSecondTerm and gitignore

--- src/LECHO.Core/ISubjectManagement.cs
+++ src/LECHO.Core/ISubjectManagement.cs
@@ -5,6 +5,8 @@ namespace LECHO.Core
     public interface ISubjectManagement
     {
         void AddSubjectToFavourite(int _UserId, int _SubjId);
+        Subjects[] GetFavouriteSubjects(int _UserId, int semester);
+        Favourites GetFavourite(int id);
         Faculties GetFaculty(int id);
         Subjects[] GetSubjects(int semester);
         Subjects[] GetSubjects(string title);
