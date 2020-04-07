fb5af8829a88d763de5dc1a838927dab51f535d5
NazarHR <47197105+NazarHR@users.noreply.github.com>
2020-04-07 17:19:14 +0300

    Added Unit tests

--- src/LECHO.Core/ISubjectManagement.cs
+++ src/LECHO.Core/ISubjectManagement.cs
@@ -6,6 +6,8 @@ namespace LECHO.Core
     {
         void AddSubjectToFavourite(int _UserId, int _SubjId);
         Faculties GetFaculty(int id);
+        Favourites GetFavourite(int id);
+        Subjects[] GetFavouriteSubjects(int _UserId, int semester);
         Subjects[] GetSubjects(int semester);
         Subjects[] GetSubjects(string title);
         Subjects[] GetSubjectsByTitle(string title, Subjects[] subjects);
