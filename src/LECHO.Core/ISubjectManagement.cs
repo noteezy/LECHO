71c78a1fccb0e7d29303f3490edf6691ec7e4b38
Vlada Baranovych <martabaranovuch@gmail.com>
2020-04-09 16:03:22 +0300

    API for deleting favs + ajax animation for add/delete favs
    * Minor fix with FavSecondTerm and gitignore
    
    * API for deleting fav + live animation of favs
    
    * Update .gitignore
    
    * fixed interface
    
    * Fixed bug with favs

--- src/LECHO.Core/ISubjectManagement.cs
+++ src/LECHO.Core/ISubjectManagement.cs
@@ -4,14 +4,13 @@ namespace LECHO.Core
 {
     public interface ISubjectManagement
     {
-        void AddSubjectToFavourite(int _UserId, int _SubjId);
-        Subjects[] GetFavouriteSubjects(int _UserId, int semester);
-        Favourites GetFavourite(int id);
         Faculties GetFaculty(int id);
         Favourites GetFavourite(int id);
-        Subjects[] GetFavouriteSubjects(int _UserId, int semester);
         Subjects[] GetSubjects(int semester);
         Subjects[] GetSubjects(string title);
         Subjects[] GetSubjectsByTitle(string title, Subjects[] subjects);
+        Subjects[] GetFavouriteSubjects(int _UserId, int semester);
+        void AddSubjectToFavourite(int _UserId, int _SubjId);
+        void DeleteSubjectFromFavourite(int _UserId, int _SubjId);
     }
 }
