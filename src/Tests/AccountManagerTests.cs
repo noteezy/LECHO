54db5f081298cbf9268f1a9e3a59a02caf4783ce
NazarHR <47197105+NazarHR@users.noreply.github.com>
2020-04-13 13:24:47 +0300

    written test pattern

--- src/Tests/AccountManagerTests.cs
+++ src/Tests/AccountManagerTests.cs
@@ -8,6 +8,7 @@ using Xunit;
 using LECHO.Infrastructure;
 using LECHO.Core;
 using static LECHO.Core.AccountManagement;
+using Autofac.Extras.Moq;
 
 namespace Tests
 {
@@ -16,6 +17,30 @@ namespace Tests
         [Fact]
         public void GetUserFirstNameTest()
         {
+            using(var mock = AutoMock.GetLoose())
+            {
+                List<Users> users = new List<Users>
+                {
+                    new Users
+                    {
+                        Login = "testLogin",
+                        FirstName = "testFirstName",
+                        MiddleName ="testMiddlrName",
+                        LastName = "testLastName"
+                    }
+                };
+                IQueryable < Users > userlist= users.AsQueryable();
+                var mockSet = new Mock<DbSet<Users>>();
+                mockSet.As<IQueryable<Users>>().Setup(x => x.Provider).Returns(userlist.Provider);
+                mockSet.As<IQueryable<Users>>().Setup(x => x.Expression).Returns(userlist.Expression);
+                mockSet.As<IQueryable<Users>>().Setup(x => x.ElementType).Returns(userlist.ElementType);
+                mockSet.As<IQueryable<Users>>().Setup(x => x.GetEnumerator()).Returns(userlist.GetEnumerator());
+                mock.Mock<LECHOContext>().SetupGet(x => x.Users).Returns(mockSet.Object);
+                var cls = mock.Create<AccountManagement>();
+                var actualUser = cls.GetUser("testLogin");
+                Assert.Equal(users[0].Login,actualUser.Login);
+                
+            }
             //AccountManagement accountManagement = new AccountManagement();
             //Users TestUser =accountManagement.GetUser("Admin");
             //Assert.Equal("this",TestUser.FirstName.ToString());
